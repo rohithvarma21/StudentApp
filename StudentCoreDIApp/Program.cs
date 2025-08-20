985147f9fbd9771c1e02d5bf21720a214b625270
rohithvarma21 <rohithvj21@gmail.com>
2025-08-20 16:51:00 -0400

    change

--- StudentCoreDIApp/Program.cs
+++ StudentCoreDIApp/Program.cs
@@ -11,7 +11,7 @@ var app = builder.Build();
 
 
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request .
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
